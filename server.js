fe3db8c8bc0dc1ddfe1590ce5ae944afa5d1aae5
Reed James <reedtjames1234@gmail.com>
2019-08-20 10:56:29 -0500

    Commit before Lecture, middleware complete

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 
+const userRouter = require("./users/users-router.js");
+
 const server = express();
 
 server.use(express.json());
+
+server.use("/users", userRouter);
 
 server.get("/", (req, res) => {
   res.send("hey, thats good news!");
--- users/users-router.js
+++ users/users-router.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
 
-const usersDb = require("./users-model.js");
+const users = require("./users-model.js");
+const restricted = require("../auth/restricted-middleware.js");
 
 const router = express.Router();
+
+router.post("/register", (req, res) => {});
 
 module.exports = router;
